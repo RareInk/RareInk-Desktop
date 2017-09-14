c0ee01a592d778c5de88c78a19dbfe2815bcd7e4
Resi Respati <resir014@gmail.com>
2017-09-15 00:21:37 +0700

    *very* rough project switcher prototype

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { NgxElectronService } from './ngx-electron';
 import { TitleBarComponent } from './title-bar';
@@ -7,7 +8,8 @@ import { ProjectSwitcherComponent } from './project-switcher/project-switcher.co
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [
     TitleBarComponent,
