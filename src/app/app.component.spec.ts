0fc78b0a7ae55587b102a07053bc48b64b2b43c7
Resi Respati <resir014@gmail.com>
2017-09-21 00:46:06 +0700

    Fixed component testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { TitleBarComponent } from './core/title-bar';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core';
 import { SharedModule } from './shared';
+import { metaReducers, reducers } from './store';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -12,7 +19,15 @@ describe('AppComponent', () => {
         AppComponent
       ],
       providers : [],
-      imports: [RouterTestingModule, CoreModule, SharedModule]
+      imports: [
+        RouterTestingModule,
+        CoreModule.forRoot(),
+        SharedModule,
+        FormsModule,
+        HttpModule,
+        StoreModule.forRoot(reducers, { metaReducers }),
+        EffectsModule.forRoot([]),
+      ]
     }).compileComponents();
   }));
 
