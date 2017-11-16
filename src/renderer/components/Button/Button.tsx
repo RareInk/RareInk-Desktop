77c483a4b34c5b95897941dc40caedac87a4a85f
Resi Respati <resir014@gmail.com>
2017-11-17 02:20:23 +0700

    initial styles for button

---
+++ src/renderer/components/Button/Button.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+interface ButtonProps {
+  id?: string;
+  className?: string;
+  type?: 'submit' | 'reset' | 'button' | 'menu';
+  kind: 'default' | 'primary' | 'success' | 'danger' | 'info' | 'warning' | 'link';
+  size?: 'small' | 'normal' | 'large';
+  onClick?: () => any;
+}
+
+class Button extends React.PureComponent<ButtonProps, {}> {
+  public render() {
+    const { id, className, type, children } = this.props;
+
+    return (
+      <button
+        id={id}
+        className={className}
+        type={type || 'button'}
+        onClick={this.props.onClick}
+      >
+        {children}
+      </button>
+    );
+  }
+}
+
+export default Button;
