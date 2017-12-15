90ecd63eeb468fb872603e02177005682411d36f
Resi Respati <resir014@gmail.com>
2017-12-15 18:42:49 +0700

    remove unused Dropdown component

--- src/renderer/components/Dropdown/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Dropdown } from './Dropdown';
-export { default as DropdownTrigger } from './DropdownTrigger';
-export { default as DropdownOptions } from './DropdownOptions';
-export { default as DropdownOption } from './DropdownOption';
